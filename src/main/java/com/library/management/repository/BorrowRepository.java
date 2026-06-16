b9f6ce7c5a640c5bdb3cacbebf809dea42742090
Gowtham T <gowthamtj0808@gmail.com>
2026-06-16 12:15:37 +0530

    Implemented Repositories

---
+++ src/main/java/com/library/management/repository/BorrowRepository.java
@@ -0,0 +1,17 @@
+package com.library.management.repository;
+
+
+import com.library.management.model.BorrowRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BorrowRepository extends JpaRepository<BorrowRecord,Long> {
+    List<BorrowRecord> findByUserId(Long id);
+    List<BorrowRecord> findByBookId(Long id);
+    Optional<BorrowRecord> findByBookIdAndReturnDataIsNull(Long bookId);
+    boolean existsByBookIdAndReturnDataIsNull(Long bookId);
+}
